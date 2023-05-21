1a238137026e38c5bb114841f3f1f1387d69d156
Simon12198 <simonkaminski006@gmail.com>
2023-05-21 17:14:48 -0400

    copy

---
+++ data/graphics/EricTerrain/Death/Death.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Death" tilewidth="16" tileheight="16" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <tile id="1">
+  <image width="16" height="16" source="Death.png"/>
+ </tile>
+</tileset>
---
+++ data/graphics/EricTerrain/Tree/Trees.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Trees" tilewidth="64" tileheight="64" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="2" height="3"/>
+ <tile id="0">
+  <image width="64" height="64" source="Tree_0.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="Tree_1.png"/>
+ </tile>
+</tileset>
